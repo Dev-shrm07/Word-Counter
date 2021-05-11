932740f040aaa0e05d6658ba26ffa9dbcd603fcb
KAVYA SHARMA <76470413+Dev-shrm07@users.noreply.github.com>
2021-05-11 14:51:50 +0530

    Update count.js

--- count.js
+++ count.js
@@ -1,15 +1,23 @@
 
+
 function print(){
 
     var stringtocount = document.getElementById('input').value;
     var arr1 = stringtocount.split(" ");
     var n = arr1.length;
-    document.getElementById('result').innerHTML = n;
-    console.log(stringtocount);
-    return n;
+    let i = 0;
+    let arr2 =[];
+    while(i<n){
+        if(arr1[i] != ""){
+            arr2.push(arr1[i]);
+        }
+        else{
+            console.log('space');
+        }
+        i++;
+    }
+    var x = arr2.length;
+    document.getElementById('result').innerHTML = x;
+    
+    
 }
-// function wordCount(str1){
-//     var arr1 = str1.split(" ");
-//     var n = arr1.length;
-//     return n;
-// }
